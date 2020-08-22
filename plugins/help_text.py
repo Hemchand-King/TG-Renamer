96b118b151c830ae7564386c88ce90520ce1647d
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-22 10:47:08 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -67,7 +67,6 @@ async def start(bot, update):
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
         reply_to_message_id=update.message_id
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('My Father',url='https://t.me/Ns_AnoNymous')]])
      )
 
 
