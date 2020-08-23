5b263ed638964c7146a1ca789952d31b3bb43d98
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-23 22:15:10 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -67,7 +67,9 @@ async def start(bot, update):
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
         reply_to_message_id=update.message_id
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('My Father 👨‍💻',url='https://t.me/Ns_AnoNymouS')]])
     )
+    return
 
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
