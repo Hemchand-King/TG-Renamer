03d90cdb86e966a770cd066ee32bc1e70c13c208
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-24 12:26:39 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -66,7 +66,9 @@ async def start(bot, update):
     TRChatBase(update.from_user.id, update.text, "/start")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT .format(message.from_user.first_name)),
+        text=Translation.START_TEXT.format(message.from_user.first_name),
+        parse_mode="html",
+        disable_web_page_preview=True,
         #reply_to_message_id=update.message_id
         reply_markup=InlineKeyboardMarkup(
        [
