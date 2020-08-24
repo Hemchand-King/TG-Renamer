b12e1d665049765640e47c0e6c020d7b2886c3ac
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-24 11:36:33 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    START_TEXT = """Hello [{m.from_user.first_name}](tg://user?id={m.chat.id}) ,
+    START_TEXT = """Hello { } ,
 This is a Telegram File convert Bot!
 You can convert any Telegram Document, using this bot! With custom thumbnail
 Please send /help to know how to use me
