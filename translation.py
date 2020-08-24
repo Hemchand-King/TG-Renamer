7a4465a9380ef3e3a6a80b62c87e58df57d075da
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-24 16:33:29 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,7 @@
 class Translation(object):
     START_TEXT = """Hello {},
-This is a Telegram Rename Bot made by @TheUnusualPsychopath.
+
+This is a Telegram Rename Bot by <a herf='https://t.me/Ns_AnoNymous'>Anonymous 👨‍💻</a>
 
 <b>Please send me any Telegram file and reply to that file with /rename New Name.mkv</b>
 
@@ -8,9 +9,9 @@ Type /help for more details."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 😎😎😎😎😎 </b>  /help for Details"
-    DOWNLOAD_START = "Downloading...😴"
-    UPLOAD_START = "Uploading...😴"
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
+    DOWNLOAD_START = "Downloading to my server.....📥"
+    UPLOAD_START = "Uploading.....📤"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations.I can't do anything for that 🤷‍♂️."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using [PROFESSOR](https://t.me/TheUnusualPsychopath)'s bot.**"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
@@ -19,7 +20,7 @@ Type /help for more details."""
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = " "
+    CUSTOM_CAPTION_UL_FILE = " @renamer23_bot"
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     HELP_USER = """Hai am Rename bot..
@@ -28,12 +29,11 @@ Type /help for more details."""
 2. Send me the file to be Renamed.
 3. Reply to that message with /rename new name.extension.
    
-<b>👉 Thank to PROFESSOR :</b> 👉 <a href="https://t.me/TheUnusualPsychopath">Owner</a>
+<b>Thanks to Anonymous :</b> 👉 <a href="https://t.me/Ns_AnoNymous">Owner 👨‍🏫</a>
 
 --------
 
-Support Group : @Brotherwoodfilmz
-| @SeriesLand4U"""
+Support Group : @anonymousbotdiscussion"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
