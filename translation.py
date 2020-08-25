aa3eb5ab2caabb3c6b93bade41c428f23b8b0dd0
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-25 16:26:43 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,6 +1,7 @@
 class Translation(object):
-    START_TEXT = """Hello <i><b>{}</b></i>
-This is a Telegram File convert Bot!
+    START_TEXT = """Hello <i><b>{}</b></i>,
+
+This is a Telegram File 2 video convert bot
 You can convert any Telegram Document, using this bot! With custom thumbnail
 Please send /help to know how to use me
 
@@ -8,25 +9,25 @@ Please send /help to know how to use me
      
     """
     ABS_TEXT = " Please don't be selfish."
-    BANNED_USER_TEXT = "you are banned"
+    BANNED_USER_TEXT = "you are misused me. So you are banned 🚫 by <a href='https://t.me/Ns_AnoNymouS'>My Father 👨‍💻<a/>\n\nSo you are not able to use me 😜"
     UPGRADE_TEXT = "No upgrade plan 🥰"
-    DOWNLOAD_START = "trying to download.....📥"
+    DOWNLOAD_START = "trying to download to my server.....📥"
     UPLOAD_START = "trying to upload.....📤"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     AFTER_SUCCESSFUL_UPLOAD_MSG = "Pleae join <a herf='https://t.me/anonymousbotsupporte'>anonymous discuss group 🗣</a>\n<a href='https://t.me/Ns_AnoNymouS'>My Father 👨‍💻</a>"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nPlease rate me if you find me useful. https://t.me/tlgrmcbostart=anydl_bot-bot \nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in next 24 hr"
+    SAVED_CUSTOM_THUMB_NAIL = "✅ Custom video / file thumbnail saved. This image will be used in next 24 hr"
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully. Trying to convert as stremable video please relax 💆‍♂️"
     CUSTOM_CAPTION_UL_FILE = "@streamblevideo_bot"
-    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found.🤷‍♂️"
     CURENT_PLAN_DETAILS = """Current plan details
 --------
 Telegram ID: <code>{}</code>
 Plan name: ________________
-Expires on: Your Death"""
+Expires on: Your Death 🛌"""
     HELP_USER = """Hi there I am a File 2 Video converted bot by <a href='https://t.me/Ns_Anonymous'>Anonymous 👨‍🏫 </a>
 <b>How to use me :</b>
          1.Send me the TG file which you \n         need to convert as file.
