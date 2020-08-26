cc34027f83ae94dac12de36fe8d3fa7aab0467c4
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-26 16:30:41 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -82,17 +82,6 @@ async def start(bot, update):
        )
      )
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
-    TRChatBase(update.from_user.id, update.text, "/upgrade")
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
-    )
 
 try:
     chat = await bot.get_chat_member(CHANNEL_USERNAME, chat_id)
@@ -116,3 +105,15 @@ except Exception:
     if edit_message:
        await reply('Some thing went wrong while checking please try later')
 return False
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/upgrade")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
