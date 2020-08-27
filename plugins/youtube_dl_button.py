fae79e972528a2f98564737e52b5f8c4853e65ef
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-27 16:18:27 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -249,7 +249,7 @@ async def youtube_dl_call_back(bot, update):
                 await bot.send_audio(
                     chat_id=update.message.chat.id,
                     audio=download_directory,
-                    caption=description,
+                    caption=description.format(current_file_name),
                     parse_mode="HTML",
                     duration=duration,
                     # performer=response_json["uploader"],
@@ -269,7 +269,7 @@ async def youtube_dl_call_back(bot, update):
                     chat_id=update.message.chat.id,
                     document=download_directory,
                     thumb=thumb_image_path,
-                    caption=description,
+                    caption=description.format(current_file_name),
                     parse_mode="HTML",
                     # reply_markup=reply_markup,
                     reply_to_message_id=update.message.reply_to_message.message_id,
@@ -299,7 +299,7 @@ async def youtube_dl_call_back(bot, update):
                 await bot.send_video(
                     chat_id=update.message.chat.id,
                     video=download_directory,
-                    caption=description.format(🤔),
+                    caption=description.format(current_file_name),
                     parse_mode="HTML",
                     duration=duration,
                     width=width,
