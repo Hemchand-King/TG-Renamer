b3f781038c15a222c24e4b403c494b7dec2d7419
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-27 15:28:28 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -37,18 +37,18 @@ async def progress_for_pyrogram(
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
-        time_to_completion = round((total - current) / speed) / 60
+        time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
 
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
-        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
+        time_to_completion = TimeFormatter(milliseconds=time_to_completion)
 
         progress = "Percentage : {0}%\n({1}{2})\n".format(round(percentage, 2),
             ''.join(["●" for i in range(math.floor(percentage / 10))]),
             ''.join(["○" for i in range(10 - math.floor(percentage / 10))])
             )
 
-        tmp = progress + "\nDone ✅ : {0} \nTotal : {1}\nSpeed 🚀 : {2}/s\Time Left ⏰: {3}min\n".format(
+        tmp = progress + "Done ✅ : {0} \nTotal : {1}\nSpeed 🚀 : {2}/s\Time Left ⏰ : {3}min\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
@@ -85,9 +85,9 @@ def TimeFormatter(milliseconds: int) -> str:
     minutes, seconds = divmod(seconds, 60)
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
-    tmp = ((str(days) + "d, ") if days else "") + \
-        ((str(hours) + "h, ") if hours else "") + \
-        ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s, ") if seconds else "") + \
-        ((str(milliseconds) + "ms, ") if milliseconds else "")
+    tmp = ((str(days) + "days, ") if days else "") + \
+        ((str(hours) + "hours, ") if hours else "") + \
+        ((str(minutes) + "min, ") if minutes else "") + \
+        ((str(seconds) + "sec, ") if seconds else "") + \
+        ((str(milliseconds) + "millisec, ") if milliseconds else "")
     return tmp[:-2]
