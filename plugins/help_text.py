53ad0154b4d65297355103d88723afbf043d6df2
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-28 16:01:56 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -78,3 +78,19 @@ async def upgrade(bot, update):
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
     )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["cancel"])
+async def cancel(bot, update):
+try:
+       if update.reply_to_message is None:
+           await bot.send_message(
+                 chat_id=update.chat.id,
+                 text="Sorry bro this command was not at available",
+                 reply_to_message_id=update.message_id,
+             )
+        else:
+             await bot.send_message(
+                 chat_id=update.chat.id,
+                 text="Sorry bro this command was not at available",
+                 reply_to_message_id=update.message_id,
+               )
