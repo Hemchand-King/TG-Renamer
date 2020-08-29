320c74f6a689e01ff7a0aa2c7967fa694a258f5d
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-29 21:03:52 +0530

    Update convert_to_video.py

--- plugins/convert_to_video.py
+++ plugins/convert_to_video.py
@@ -46,7 +46,7 @@ async def convert_to_video(bot, update):
         download_location = Config.DOWNLOAD_LOCATION + "/"
         a = await bot.send_message(
             chat_id=update.chat.id,
-            text="Analyzing File....😁,
+            text="Analyzing File....😁",
             reply_to_message_id=update.message_id
         )
         c_time = time.time()
