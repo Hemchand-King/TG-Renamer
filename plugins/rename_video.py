dd3d8f5a3744e47cbb13a7a121b9b544394150bd
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-30 12:29:53 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -75,7 +75,7 @@ async def rename_video(bot, update):
             except:
                 pass
             new_file_name = download_location + file_name
-            os.rename(the_real_download_location, new_file_name)
+            os.rename_video(the_real_download_location, new_file_name)
             await bot.edit_message_text(
                 text=Translation.UPLOAD_START_VIDEO,
                 chat_id=update.chat.id,
