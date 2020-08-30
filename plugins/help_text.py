e727fb5adc5fb056179330a569b3b8358dd05f38
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-30 11:16:18 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -101,7 +101,7 @@ async def settings(bot, update):
                 [  # First row
                     InlineKeyboardButton(  # Generates a callback query when pressed
                         "Button",
-                        callback_data="data"
+                        callback_data="start"
                   )
                 ]
              ]
