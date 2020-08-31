5b3ae52da31f99b72f2fd074c59595bc657bfe7b
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-31 16:05:26 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -132,7 +132,7 @@ Owner_id = [1337144652]
 from sample_config import Config
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["ban"]))
-async def ban(bot, update):
+async def ban(bot, update, Owner_id):
    TRChatBase(update.from_user.id, update.text, "/ban")
    banid = int(update.text.split(' ', 1)[1])
    if update.from_user.id in Owner_id:
@@ -143,3 +143,19 @@ async def ban(bot, update):
 
    return Config.BANNED_USERS.append(banid)
 
+@pyrogram.Client.on_message(pyrogram.Filters.command(["unban"]))
+async def ban(bot, update, Owner_id):
+   TRChatBase(update.from_user.id, update.text, "/unban")
+   unbanid = int(update.text.split(' ', 1)[1])
+   if update.from_user.id in Owner_id:
+     if unbanid in BANNED_USERS:
+      await bot.send_message(
+        chat_id=update.chat.id,
+        text='User with ID {} Was unbanned and free to use  your bot'.format(unbanid)
+        )
+     return Config.BANNED_USERS.append(unbanid)
+     else:
+       await bot.send_message(
+        chat_id=update.chat.id,
+        text='User with ID {} Was not an banned user'.format(unbanid)
+        )
