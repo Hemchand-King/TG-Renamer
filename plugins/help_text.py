33d97dd4eb67d20af85d6f14b7b387f29677fd25
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-31 11:12:02 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -3,6 +3,21 @@
 # (c) Shrimadhav U K
 
 # the logging things
+
+
+from pyrogram import Client, Filters
+
+from helper_funcs.chat_base import TRChatBase
+from helper_funcs.display_progress import progress_for_pyrogram
+
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+# https://stackoverflow.com/a/37631799/4723940
+from PIL import Image
+
+
+
+
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
