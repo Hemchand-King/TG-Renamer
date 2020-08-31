5a91c0562f6e8b98953f10efff03fd3f26b306b3
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-08-31 10:57:46 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -112,13 +112,15 @@ async def donate(bot, update):
            )
           )
 
+from sample_config import BANNED_USERS
+
 @pyrogram.Client.on_message(pyrogram.Filters.command(["ban"]))
   banid = update.text.split(" ", 1)
    if update.from_user.id is '1337144652':
       await bot.send_message(
         chat_id=update.chat.id,
-        text='User with ID {} Was banned from using your bot successfully'.formate(banid),
-        
+        text='User with ID {} Was banned from using your bot successfully'.formate(banid)
+        )
 
-   return BANNED_USERS.apend
+   return sample_config.BANNED_USERS.apend
 
