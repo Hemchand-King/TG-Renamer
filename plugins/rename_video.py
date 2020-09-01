4fc8ff06d545262cd16c155fe7ba0155d5a34073
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-01 09:02:29 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -109,7 +109,6 @@ async def rename_video(bot, update):
                 video=new_file_name,
                 thumb=thumb_image_path,
                 caption=description,
-                duration=duration,
                 # reply_markup=reply_markup,
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
