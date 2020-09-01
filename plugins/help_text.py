757e139c2ca23941680d3b7900c4a465d2c71805
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-01 10:33:54 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -132,7 +132,7 @@ async def ban(bot, update):
         chat_id=update.chat.id,
         text="""Hai 😡 **{}** your not any admin this command only for admin of this bot for banning users from this bot""".format(update.from_user.first_name),
         parse_mode='Markdown'
-      )
+     )
      return False
 
 from sample_config import Config
