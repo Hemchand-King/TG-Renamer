c5e0c4ddb6a365bcc8ad996ff9c5a6da980d7b22
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-02 19:47:20 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -184,3 +184,14 @@ async def unban(bot, update):
           parse_mode='Markdown'
        )
       return False
+from pyrogram import ForceReply
+
+@pyrogram.Client.on_message(pyrogram.Filters.document)
+async def doc(bot, update):
+         await bot.send_message(
+              chat_id=update.chat.id,
+              text="""**Now Send me the name of the new file with extension**\n if you want to upload as file send in this format `New Name.Extention`
+If you want to upload as video send the name in this format `video - New Name.extention`"""
+              parse_mode='markdown',
+              reply_markup=ForceReply(),
+              quote=True)
