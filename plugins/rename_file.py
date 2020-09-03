cf953cc9c5d243ff9f1225f98d036bea8606298c
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-03 07:07:50 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -58,7 +58,7 @@ If you want to upload as video send the name in this format
     if update.from_user.id in Config.BANNED_USERS:
         await update.reply_text("You are B A N N E D")
         return
-     TRChatBase(update.from_user.id, update.text, "rename")
+    TRChatBase(update.from_user.id, update.text, "rename")
      if (" " in update.text) and (update.reply_to_message.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
         if len(file_name) > 6400:
