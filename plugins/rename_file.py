2e27281ae3671dae26631b429f5d54cfbde5a57d
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-03 07:01:11 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -23,6 +23,7 @@ from translation import Translation
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import Client, Filters
+from pyrogram import ForceReply
 
 from helper_funcs.chat_base import TRChatBase
 from helper_funcs.display_progress import progress_for_pyrogram
