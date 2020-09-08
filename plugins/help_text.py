cb4cf2ec5c0d69074bafa0964e0da803f6852a03
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-08 20:04:52 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -82,23 +82,15 @@ async def upgrade(bot, update):
     )
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["setting"]))
+@pyrogram.Client.on_message(pyrogram.Filters.document & incoming)
 async def settings(bot, update):
                await bot.send_message(
                      chat_id=update.chat.id,
                      text="Sorry bro I cannot cancel this process now 😔",
                      #reply_to_message_id=update.message_id,
-                     reply_markup=InlineKeyboardMarkup(
-             [
-                [  # First row
-                    InlineKeyboardButton(  # Generates a callback query when pressed
-                        "Button",
-                        callback_data="start"
-                  )
-                ]
-             ]
+                     reply_markup=ForceReply()
            )
-         )
+         
 @pyrogram.Client.on_message(pyrogram.Filters.command(["donate"]))
 async def donate(bot, update):
        await bot.send_message(
@@ -176,27 +168,3 @@ async def unban(bot, update):
           parse_mode='Markdown'
        )
       return False
-CHANNEL_USERNAME = "@anonymousbotupdates"
-async def user_in_channel(CHANNEL_USERNAME):
- try:
-    chat = await bot.get_chat_member(CHANNEL_USERNAME, chat_id)
-    if chat.status=='kicked':
-      if edit_message:
-         await reply('😡 hai {} you are banned you are not able to use me').format(update.from_user.first_name)
-      return False
-    else:
-      return True
- except UserBannedInChannel:
-    if edit_message:
-       await reply("Hai {} you made a mistake so you are banned from channel so you are banned from me too 😜").format(update.from_user.first_name)
-
- except UserNotParticipant:
-    if edit_message:
-       button = [[InlineKeyboardButton('join Updates channel 📣', url='https://t.me/anonymousbotupdates')]]
-       markup = InlineKeyboardMarkup(button)
-       await reply("""Hai bro you must join my channel for using my bot""",  reply_markup=markup)
- except Exception:
-    LOGGER.exception('Unable to verify user')
-    if edit_message:
-       await reply('Some thing went wrong while checking please try later')
- return False
