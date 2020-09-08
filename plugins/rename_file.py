79e828ac7fa661054bbdabcd3f24a0083b6467b4
naveen9329 <69340807+naveen9329@users.noreply.github.com>
2020-09-08 16:02:35 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -42,8 +42,8 @@ async def rename_doc(bot, update):
     TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
-        if len(file_name) > 64:
         new_file = file_name[:60] + file_name[-4:]
+      if len(file_name) > 64:
         description = Translation.CUSTOM_CAPTION_UL_FILE
         download_location = Config.DOWNLOAD_LOCATION + "/"
         a = await bot.send_message(
